1558b739e94eecd466697b825a3b5c31e6c216aa
hitigon <hitigon@gmail.com>
2013-05-13 01:25:52 -0400

    del sh

--- streamcluster/awktime.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "GPU Time of Kernels:"
-awk '$2!="memcpyDtoH" && $2!="memcpyHtoD" {sum+=$5} END {print sum*10e-6}' cuda_profile_0.log
-echo "GPU Time of Memory:"
-awk '$2=="memcpyDtoH" || $2=="memcpyHtoD" {sum+=$5} END {print sum*10e-6}' cuda_profile_0.log
-echo "CPU Time of Kernels:"
-awk '$2!="memcpyDtoH" && $2!="memcpyHtoD" {sum+=$8} END {print sum*10e-6}' cuda_profile_0.log
-echo "CPU Time of Memory:"
-awk '$2=="memcpyDtoH" || $2=="memcpyHtoD" {sum+=$8} END {print sum*10e-6}' cuda_profile_0.log
